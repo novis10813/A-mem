96f34fe3e41e386fe1d8b04ba7fe945488303da5
novis10813 <zhizhongz895253@gmail.com>
2026-07-07 11:25:43 +0800

    feat: normalize amem memories to benchmark stores

---
+++ src/amem/methods/__init__.py
@@ -0,0 +1,1 @@
+"""Method adapters for benchmark components."""
---
+++ src/amem/methods/amem/__init__.py
@@ -0,0 +1,5 @@
+"""A-Mem benchmark adapters."""
+
+from .serialization import memories_to_store, memory_note_to_record
+
+__all__ = ["memories_to_store", "memory_note_to_record"]
